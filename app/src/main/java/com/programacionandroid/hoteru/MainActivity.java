8388d19127c1c709dab89ffcc3226436fcd98c27
unknown <Laboratorio@OV68PCALU-L1109.alumnos.cst>
2022-09-28 16:13:42 -0300

    Inicio de proyecto.

---
+++ app/src/main/java/com/programacionandroid/hoteru/MainActivity.java
@@ -0,0 +1,14 @@
+package com.programacionandroid.hoteru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
