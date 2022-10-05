34582befaa43f6d7e3401cec3767685bcdd916c3
JoseAngel99 <j.lafferte@alumnos.santotomas.cl>
2022-10-05 16:41:27 -0300

    Version 1.9

--- app/src/main/java/com/programacionandroid/hoteru/activity_menu_reservar.java
+++ app/src/main/java/com/programacionandroid/hoteru/activity_menu_reservar.java
@@ -10,7 +10,7 @@ public class activity_menu_reservar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_reservar);
     }
 
     public void cambiarActivityInfoHabitacion(View view){
