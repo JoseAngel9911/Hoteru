77b9e44ec557b0ea686313aacd76a7e2a5e6ef4d
coff2804 <paulinichi.borquez@gmail.com>
2022-10-05 16:43:59 -0300

    Version 1.9

--- app/src/main/java/com/programacionandroid/hoteru/activity_register.java
+++ app/src/main/java/com/programacionandroid/hoteru/activity_register.java
@@ -1,6 +1,16 @@
 package com.programacionandroid.hoteru;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
 }
