0ab187806455e3e8aed17f4a944c18927b06d338
JoseAngel99 <j.lafferte@alumnos.santotomas.cl>
2022-10-05 16:01:28 -0300

    Version 1.9

--- app/src/main/java/com/programacionandroid/hoteru/activity_cantidad.java
+++ app/src/main/java/com/programacionandroid/hoteru/activity_cantidad.java
@@ -3,4 +3,9 @@ package com.programacionandroid.hoteru;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_cantidad extends AppCompatActivity {
+
+    /*protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }*/
 }
