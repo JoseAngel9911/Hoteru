cdb8e13ad798918d0c668169f5018644ca572c26
JoseAngel99 <j.lafferte@alumnos.santotomas.cl>
2022-10-05 16:48:56 -0300

    Version 1.9

--- app/src/main/java/com/programacionandroid/hoteru/activity_menu_serviciohabitacion.java
+++ app/src/main/java/com/programacionandroid/hoteru/activity_menu_serviciohabitacion.java
@@ -1,6 +1,8 @@
 package com.programacionandroid.hoteru;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,17 @@ public class activity_menu_serviciohabitacion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_sevicioahabitacion);
     }
+
+    public void cambiarActivityServicioComida(View view){
+        Intent activity = new Intent(this,activity_servicio_comida.class);
+        startActivity(activity);
+    }
+
+
+    public void cambiarActivityServicioLimpieza(View view){
+        Intent activity = new Intent(this,activity_servicio_limpieza.class);
+        startActivity(activity);
+    }
+
 
 }
